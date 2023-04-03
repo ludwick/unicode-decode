00ddfe7c822449a73a8ea41821457dc37b8776c5
Rachael Ludwick <rachael@ilkjar.com>
2023-04-03 13:51:41 -0700

    Read from argument or stdin

--- README.md
+++ README.md
@@ -9,13 +9,18 @@ limited to UTF-8 input.
 Pass in a string as an argument:
 
 ```
-unicode-decode -t "some interesting string"
+unicode-decode "some interesting string"
 ```
 
 Or from standard input:
 
 ```
-echo "some interesting string" | unicode-decode -
+echo "some interesting string" | unicode-decode
+```
+
+```
+unicode-decode<enter>
+some interesting string
 ```
 
 To add URLs to useful websitese for a given string:
@@ -26,7 +31,7 @@ echo "what" | unicode-decode -u -
 
 Options:
 
-* `-t` / `--text`: string to process
+* Positional arguments: string to process
 * `-u` / `--urls`: include urls in output
 * `-e` / `--encoding`: the type of data (only allowed: utf8)
 * `-h` / `--help`: print help message
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,62 @@
-use clap::{Parser, ValueEnum};
+use clap::{Parser, ValueEnum, CommandFactory};
+use clap::error::ErrorKind;
+use std::io;
 
 
 // allowed input encodings
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum InputEncoding {
+    /// UTF-8 directly pasted or typed from somewhere
     Utf8,
 }
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-struct Args {
-    /// text to process
-    #[arg(short, long)]
-    text: String,
-
+/// some help text
+struct Cli {
     /// whether to show urls
     #[arg(short, long, default_value_t = false)]
     urls: bool,
 
-    /// encoding to assume
-    #[arg(short, long, value_enum)]
+    /// encoding to assume on input text
+    #[arg(short, long, value_enum, default_value_t = InputEncoding::Utf8)]
     encoding: InputEncoding,
+
+    /// Text to process
+    #[arg(required = false)]
+    text: Option<String>,
 }
 
 fn main() {
-    let args = Args::parse();
+    let cli = Cli::parse();
+    let mut buffer = String::new();
+    let stdin = io::stdin();
 
-    println!("text: {}", args.text);
-    println!("urls: {}", args.urls);
-    match args.encoding {
+    println!("urls: {}", cli.urls);
+    match cli.encoding {
         InputEncoding::Utf8 => {
             println!("encoding: utf8");
+        }
+    }
+
+    match cli.text {
+        Some(text) => {
+            println!("text: {}", text);
+        },
+        None => {
+            match stdin.read_line(&mut buffer) {
+                Ok(text) => {
+                    println!("text (stdin): {}", buffer);
+                },
+                Err(_) => {
+                    let mut cmd = Cli::command();
+                    cmd.error(
+                        ErrorKind::MissingRequiredArgument,
+                        "Must provide arguments or standard input",
+                    )
+                    .exit();
+                }
+            }
         }
     }
 }
