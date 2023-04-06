7e097e6f74e81371aba0c094523c585ffbe13e27
Rachael Ludwick <rachael@ilkjar.com>
2023-04-05 17:05:30 -0700

    Add looking up char name using ucd-generate
    Use ucd-generate and unicode 15 data files to generate a names to code
    point table that at runtime is loaded into a hashmap. Right now only
    knows names of printable characters.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # unicode-decode
 
-Command-line tool to decode &amp; explain unicode strings. This is just here
-to learn a bit about Rust and how it manipulates Unicode strings. Currently
-limited to UTF-8 input.
+Command-line tool to decode &amp; explain unicode strings. This repository is
+not meant to be anything anyone should depend on, but is instead a useful to
+me tool I decided to write as a means of refreshing my knowledge of Rust (previous
+work was under Edition 2018!) and also to understand Unicode better.
+This is currently limited to analyzing UTF-8 input.
 
 ## Usage
 
@@ -17,6 +19,9 @@ Or from standard input:
 ```
 echo "some interesting string" | unicode-decode
 ```
+
+If you don't pass in input by one of these mechanisms, the tool will wait for
+input followed by a newline:
 
 ```
 unicode-decode<enter>
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use cli_table::{format::Justify, print_stdout, Table, WithTitle};
 use itertools::Itertools;
 use std::io;
 use std::vec::Vec;
+use std::collections::HashMap;
+
+pub mod names;
+
 
 // allowed input encodings
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -33,15 +37,31 @@ struct Cli {
 struct CodeUnit {
     #[table(title = "Display", justify = "Justify::Left")]
     display: String,
-    #[table(title = "Code Unit")]
+    #[table(title = "Code Unit", justify = "Justify::Right")]
     code_unit: String,
-    #[table(title = "Name")]
+    #[table(title = "Name", justify = "Justify::Left")]
     name: String,
-    #[table(title = "UTF-8 Byte(s)")]
+    #[table(title = "UTF-8 Byte(s)", justify = "Justify::Right")]
     utf8_bytes: String,
     #[table(title = "Links")]
     urls: String,
 }
+
+struct NameDatabase {
+    db: HashMap<u32, &'static str>
+}
+
+impl NameDatabase {
+    pub fn new() -> NameDatabase {
+        let iter = names::NAMES.into_iter().map(|(name, code)| (*code, *name));
+        NameDatabase { db: HashMap::from_iter(iter) }
+    }
+
+    pub fn for_char(&self, c: char) -> Option<&str> {
+        self.db.get(&(c as u32)).copied()
+    }
+}
+
 
 fn char_to_bytestring(c: char) -> String {
     let mut buf = [0; 4];
@@ -50,12 +70,13 @@ fn char_to_bytestring(c: char) -> String {
 }
 
 fn build_table(text: &String) -> Vec<CodeUnit> {
+    let names = NameDatabase::new();
     let mut result = Vec::new();
     for val in text.chars() {
         result.push(CodeUnit {
             display: String::from(val),
-            code_unit: String::from("tbd"),
-            name: String::from("tbd"),
+            code_unit: format!("{:04x}", val as u32),
+            name: String::from(match names.for_char(val) { Some(v) => v, None => "unknown" }),
             utf8_bytes: char_to_bytestring(val),
             urls: String::from("tbd"),
         });
@@ -80,7 +101,9 @@ fn main() {
         Some(text) => text,
         None => {
             // TODO: how to make it error on an empty string??
-            // TODO: make it remove the line separator
+            // TODO: make it remove the line separator if present (unclear
+            // but when interactively waiting, the newline seems to be removed
+            // so maybe it's just an EOF in that case?)
             match stdin.read_line(&mut buffer) {
                 Ok(_) => buffer,
                 Err(_) => {
